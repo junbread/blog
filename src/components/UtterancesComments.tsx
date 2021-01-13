df27ac238c55d1a84ce9e83dd7c7ed5d40524bba
Junhyun Kim <me@junbread.win>
2021-01-13 23:49:23 +0900

    댓글 기능 추가

---
+++ src/components/UtterancesComments.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const UtterancesComments: React.FC = () => (
+  <section
+    ref={elem => {
+      if (!elem) {
+        return
+      }
+      const scriptElem = document.createElement('script')
+      scriptElem.src = 'https://utteranc.es/client.js'
+      scriptElem.async = true
+      scriptElem.crossOrigin = 'anonymous'
+      scriptElem.setAttribute('repo', 'junbread/blog')
+      scriptElem.setAttribute('issue-term', 'pathname')
+      scriptElem.setAttribute('label', 'blog-comment')
+      scriptElem.setAttribute('theme', 'github-light')
+      elem.appendChild(scriptElem)
+    }}
+  />
+)
+
+export default UtterancesComments
